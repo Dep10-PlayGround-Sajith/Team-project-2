dc4de33b84aeb73a13c51028857f384174978c1a
nishan akalanka <hdnakalanka@gmail.com>
2023-03-23 19:04:13 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep10/app/controller/ManageStudentSceneController.java
@@ -0,0 +1,153 @@
+package lk.ijse.dep10.app.controller;
+
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.AnchorPane;
+import lk.ijse.dep10.app.db.db.DBConnection;
+import lk.ijse.dep10.app.model.Student;
+
+import java.sql.*;
+import java.time.LocalDate;
+
+public class ManageStudentSceneController {
+
+    public Button btnSave;
+    public Button btnDelete;
+    public AnchorPane root;
+    @FXML
+    private Button btnNewStuden;
+
+    @FXML
+    private TableView<Student> tblStudent;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtName;
+
+    public void initialize() {
+
+        btnDelete.setDisable(true);
+
+//        txtSearch.textProperty().addListener((value,previous,current)->{
+//            Connection connection= DBConnection.getInstance().getConnection();
+//            try {
+//                Statement stm=connection.createStatement();
+//                String sql="SELECT * FROM Student WHERE first_name LIKE '%1$s' OR last_name LIKE '%1$s'";
+//                sql = String.format(sql,"%"+current+"%");
+//                ResultSet rst=stm.executeQuery(sql);
+//
+//                ObservableList<Student> studentList=tblStudents.getItems();
+//                studentList.clear();
+//                while (rst.next()){
+//                    int id=rst.getInt("id");
+//                    String firstName=rst.getString("first_name");
+//                    String lastName=rst.getString("last_name");
+//                    String address=rst.getString("address");
+//                    Gender gender=Gender.valueOf(rst.getString("gender"));
+//                    LocalDate dob=rst.getDate("dob").toLocalDate();
+//                    studentList.add(new Student(id,firstName,lastName,address,gender,dob));
+//                }
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        });
+
+        /* Column mapping */
+        tblStudent.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
+        tblStudent.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+        tblStudent.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("address"));
+
+
+        loadAllStudents();
+
+        /* Setup keyboard shortcuts */
+        Platform.runLater(() -> {
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.N, KeyCombination.CONTROL_ANY), () -> btnNewStuden.fire());
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_ANY), () -> btnNewStuden.fire());
+            root.getScene().getAccelerators().put(new KeyCodeCombination(KeyCode.F1), () -> tblStudent.requestFocus());
+        });
+
+        tblStudent.getSelectionModel().selectedItemProperty().addListener((observableValue, student, current) -> {
+            btnDelete.setDisable(current==null);
+            if (current==null)return;
+
+            txtId.setText(current.getId()+"");
+            txtName.setText(current.getName());
+            txtAddress.setText(current.getAddress());
+        });
+    }
+    private void loadAllStudents(){
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            Statement stm=connection.createStatement();
+            ResultSet rst=stm.executeQuery("SELECT * FROM Student");
+            ObservableList<Student> studentList=tblStudent.getItems();
+
+
+            while (rst.next()){
+                String id=rst.getString("id");
+                String name=rst.getString("name");
+                String address=rst.getString("address");
+
+
+                studentList.add(new Student(id, name, address));
+            }
+
+            Platform.runLater(()->btnNewStuden.fire());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,"Failed to load Student details").showAndWait();
+            Platform.exit();
+        }
+    }
+    private boolean isDataValid(){
+        boolean isDataValid=true;
+        for(Node node:new Node[]{txtName,txtAddress,txtId}){
+            node.getStyleClass().remove("invalid");
+        }
+        String name=txtName.getText( );
+        String address=txtAddress.getText( );
+
+        if (address.strip().length()<3){
+            isDataValid=false;
+            txtAddress.requestFocus();
+            txtAddress.selectAll();
+            txtAddress.getStyleClass().add("invalid");
+        }
+
+        if (!name.strip().matches("[A-Za-z]+")){
+            isDataValid=false;
+            txtName.getStyleClass().add("invalid");
+            txtName.selectAll();
+            txtName.requestFocus();
+
+        }
+        return isDataValid;
+    }
+
+
+    @FXML
+    void btnNewStudentOnAction(ActionEvent event) {
+
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+    }
+
+    public void btnSaveOnAction(ActionEvent event) {
+    }
+}
